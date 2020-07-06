d5dca09056fd39d81e090cc1b96ebf51e9086491
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-07-06 17:23:02 -0500

    Add slide show progress circles to bottom of container

--- image-slider/script.js
+++ image-slider/script.js
@@ -1,60 +1,47 @@
-// when arrow is clicked, move to next sibling, if no next sibling, go back to start
-
 const autoSlideshow = function() {
-  const imgs = document.getElementsByClassName('img');
-  Array.from(imgs).forEach((img) => {
-    if(img.id === 'current') {
-      console.log('doggie');
-      setTimeout(function() {
-        if(!img.nextElementSibling) {
-          console.log('aay');
-          img.removeAttribute('id');
-          imgs[0].id = 'current';
-          autoSlideshow();
-        };
-      }, 5000);
-      if(img.nextElementSibling) {
-        setTimeout(function() {
-          img.removeAttribute('id');
-          img.nextElementSibling.id = 'current';
-          autoSlideshow();
-        }, 5000);
-      };
-    };
-  });
+  setTimeout(function() {
+    nextImg();
+    autoSlideshow();
+  }, 5000);
 };
 
+
 const nextImg = function() {
-  const nextImgBtn = document.getElementById('next-img-btn');
   const imgArray = Array.from(document.getElementsByClassName('img'));
-  nextImgBtn.addEventListener('click', () => {
-    let currentImg = document.getElementById('current');
-    if(!currentImg.nextElementSibling) {
-      currentImg.removeAttribute('id');
-      imgArray[0].id = 'current';
-    } else {
-      currentImg.removeAttribute('id');
-      currentImg.nextElementSibling.id = 'current';
-    }
-  });
+  let currentImg = document.getElementById('current');
+  if(!currentImg.nextElementSibling) {
+    currentImg.removeAttribute('id');
+    imgArray[0].id = 'current';
+  } else {
+    currentImg.removeAttribute('id');
+    currentImg.nextElementSibling.id = 'current';
+  }
 };
 
 const previousImg = function() {
-  const previousImgBtn = document.getElementById('previous-img-btn');
   const imgArray = Array.from(document.getElementsByClassName('img'));
-  previousImgBtn.addEventListener('click', () => {
-    let currentImg = document.getElementById('current');
-    if(!currentImg.previousElementSibling) {
-      currentImg.removeAttribute('id');
-      imgArray[imgArray.length - 1].id = 'current';
+  let currentImg = document.getElementById('current');
+  if(!currentImg.previousElementSibling) {
+    currentImg.removeAttribute('id');
+    imgArray[imgArray.length - 1].id = 'current';
+  } else {
+    currentImg.removeAttribute('id');
+    currentImg.previousElementSibling.id = 'current';
+  };
+};
 
-    } else {
-      currentImg.removeAttribute('id');
-      currentImg.previousElementSibling.id = 'current';
-    };
+const controls = function () {
+  const nextImgBtn = document.getElementById('next-img-btn');
+  nextImgBtn.addEventListener('click', () => {
+    nextImg();
+  });
+  
+  const previousImgBtn = document.getElementById('previous-img-btn');
+  previousImgBtn.addEventListener('click', () => {
+    previousImg();
   });
 };
 
-previousImg();
-nextImg();
-//autoSlideshow();
+
+controls();
+autoSlideshow();

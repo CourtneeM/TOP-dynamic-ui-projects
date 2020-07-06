ba0b3483323e26634bebb0a2ccb32f4ec0f7792b
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-07-06 18:52:17 -0500

    Add working progress circles that allow for image selection

--- image-slider/script.js
+++ image-slider/script.js
@@ -1,33 +1,63 @@
-const autoSlideshow = function() {
-  setTimeout(function() {
+const autoSlideshow = function () {
+  setTimeout(function () {
     nextImg();
     autoSlideshow();
   }, 5000);
 };
 
 
-const nextImg = function() {
+const nextImg = function () {
   const imgArray = Array.from(document.getElementsByClassName('img'));
+  const progressCircles = Array.from(document.getElementById('img-progress').children);
   let currentImg = document.getElementById('current');
+  let imgIndex = (Array.from(currentImg.parentNode.children)).indexOf(currentImg);
   if(!currentImg.nextElementSibling) {
     currentImg.removeAttribute('id');
     imgArray[0].id = 'current';
+    progressCircles[0].id = 'current-slide-circle';
+    progressCircles[imgIndex].removeAttribute('id');
   } else {
     currentImg.removeAttribute('id');
     currentImg.nextElementSibling.id = 'current';
+    progressCircles[imgIndex].removeAttribute('id');
+    progressCircles[imgIndex + 1].id = 'current-slide-circle';
   }
 };
 
-const previousImg = function() {
+const previousImg = function () {
   const imgArray = Array.from(document.getElementsByClassName('img'));
+  const progressCircles = Array.from(document.getElementById('img-progress').children);
   let currentImg = document.getElementById('current');
+  let imgIndex = (Array.from(currentImg.parentNode.children)).indexOf(currentImg);
   if(!currentImg.previousElementSibling) {
     currentImg.removeAttribute('id');
     imgArray[imgArray.length - 1].id = 'current';
+    progressCircles[progressCircles.length - 1].id = 'current-slide-circle';
+    progressCircles[imgIndex].removeAttribute('id');
   } else {
     currentImg.removeAttribute('id');
     currentImg.previousElementSibling.id = 'current';
+    progressCircles[imgIndex].removeAttribute('id');
+    progressCircles[imgIndex - 1].id = 'current-slide-circle';
   };
+};
+
+const pickImage = function () {
+  const imgArray = Array.from(document.getElementsByClassName('img'));
+  const progressCircles = document.getElementById('img-progress');
+  progressCircles.addEventListener('click', (e) => {
+    if(e.target.tagName === 'P') {
+      let index = Array.from(e.target.parentNode.children).indexOf(e.target);
+      Array.from(progressCircles.children).forEach((circle) =>  {
+        circle.removeAttribute('id');
+      });
+      imgArray.forEach((img) => {
+        img.removeAttribute('id');
+      });
+      Array.from(progressCircles.children)[index].id = 'current-slide-circle';
+      imgArray[index].id = 'current';
+    };
+  });
 };
 
 const controls = function () {
@@ -42,6 +72,6 @@ const controls = function () {
   });
 };
 
-
+pickImage();
 controls();
 autoSlideshow();
